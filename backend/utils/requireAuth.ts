970fc05b4c77a61c675482569b39c0f0cc528e6f
Parth Agrawal <pvagrawal0@gmail.com>
2024-07-11 15:35:30 -0400

    middleware creates a new user

--- backend/utils/requireAuth.ts
+++ backend/utils/requireAuth.ts
@@ -1,5 +1,6 @@
 import express, { type NextFunction } from "express";
 import { clerkClient } from '@clerk/clerk-sdk-node';
+import prisma from "../prisma/client";
 
 export const requireAuth = async (
     req: express.Request,
@@ -17,9 +18,40 @@ export const requireAuth = async (
     try {
         const verifiedToken = await clerkClient.verifyToken(authToken, { jwtKey: process.env.CLERK_JWT_KEY });
         console.log('verifiedToken', verifiedToken);
+
+        const dbUser = await prisma.adminUser.findUnique({
+            where: {
+                clerkId: verifiedToken.sub
+            }
+        })
+        if (!dbUser) {
+            const clerkUser = await clerkClient.users.getUser(verifiedToken.sub);
+            const email = clerkUser.emailAddresses[0].emailAddress;
+            const name = clerkUser.fullName || "";
+
+            const newUser = await prisma.adminUser.create({
+                data: {
+                    clerkId: verifiedToken.sub,
+                    email: email,
+                    name: name,
+                }
+            })
+
+            console.log('newAdminUser', newUser);
+
+        }
+
         next();
+
+
     } catch (error) {
         console.error('Error verifying JWT:', error);
         res.status(401).json({ message: "Unauthorized bro" });
     }
+
+
+
+
+    next();
+
 };
